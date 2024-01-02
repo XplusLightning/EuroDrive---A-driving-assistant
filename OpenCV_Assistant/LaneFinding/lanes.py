5494221f189e827f827e2843573191e78b8e9add
Arya B <99092346+XplusLightning@users.noreply.github.com>
2024-01-02 15:58:59 +0000

    OpenCV Lane Detection v1.0

---
+++ OpenCV_Assistant/LaneFinding/lanes.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+from OpenCV_Assistant.LaneFinding.GeneralCVFunctions import GeneralFunctions
+
+class LaneFinding:
+    def __init__(self):
+        pass
+    
+    
+    
+    
